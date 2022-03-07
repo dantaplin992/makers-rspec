ab23e193ebb7dce016300a4e98b842dcd05919a3
Daniel Taplin <98479830+dantaplin992@users.noreply.github.com>
2022-03-07 17:01:10 +0000

    present

--- spec/present_spec.rb
+++ spec/present_spec.rb
@@ -1,7 +1,14 @@
 require 'present'
 
 RSpec.describe 'present' do
-  it '' do
-    
+  it 'fails to wrap if contents are not nil' do
+    present = Present.new
+    present.wrap("a")
+    expect{ present.wrap("b") }.to raise_error "A contents has already been wrapped."
+  end
+
+  it 'fails to unwrap if contents are nil' do
+    present = Present.new
+    expect { present.unwrap }.to raise_error "No contents have been wrapped."
   end
 end
